c06da0ef51aa48504cfcf66741fe69e3d07a14b9
Juan Rios <juanrios@tuta.io>
2025-03-15 23:56:45 -0500

    add: now reader works in spanish or english using the flag -l ['es', 'en']

--- wikipediaReader.py
+++ wikipediaReader.py
@@ -5,6 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import re
+import sys 
 
 import sounddevice as sd
 from kokoro_onnx import Kokoro
@@ -14,16 +15,16 @@ import asyncio
 import onnxruntime as ort
 ort.set_default_logger_severity(3)
 
-async def main(txt: str):
+async def reader(txt: str, voice: str, lang: str):
     print("\nLoading model...")
     kokoro = Kokoro("kokoro-v1.0.onnx", "voices-v1.0.bin")
     print("Model loaded")
     print("Creating stream...")
     stream = kokoro.create_stream(
         txt,
-        voice="af_bella",
+        voice=voice,
         speed=1.0,
-        lang="en-us",
+        lang=lang,
     )
 
     count = 0
@@ -34,89 +35,117 @@ async def main(txt: str):
         sd.play(samples, sample_rate)
         sd.wait()
 
-url_base = 'https://en.wikipedia.org/wiki/'
+def main(language):
+    url_base = 'https://en.wikipedia.org/wiki/'
+    if language=='es':
+        url_base = 'https://es.wikipedia.org/wiki/'
+    success = False
+    while True:
+        topic = input('\nWhich topic would you like to look for?[q:quit] ')
+        if topic=='q':
+            print("Have a nice day, human!")
+            break
+        start_time = time.time()
+        topic = topic.split()
+        for idx,word in enumerate(topic):
+            topic[idx]= word.casefold()[0].capitalize()+word.casefold()[1:]
+        topic ='_'.join(topic)
+        
+        response = requests.get(url_base+topic)
 
-success = False
-while True:
-    topic = input('\nWhich topic would you like to look for?[q:quit] ')
-    if topic=='q':
-        print("Have a nice day, human!")
-        break
-    start_time = time.time()
-    topic = topic.split()
-    for idx,word in enumerate(topic):
-        topic[idx]= word.casefold()[0].capitalize()+word.casefold()[1:]
-    topic ='_'.join(topic)
-    
-    response = requests.get(url_base+topic)
+        # Check if the status code is 200, indicating a successful request
+        if response.status_code == 200:
+            print("\nPage: {0}".format(url_base+topic))
+            print(f"fetched in {time.time() - start_time} seconds")
+            success =True
+        else:
+            print("Unable to find the topic, try again\n")
+        out = False
+        if success:
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Check if the status code is 200, indicating a successful request
-    if response.status_code == 200:
-        print("\nPage: {0}".format(url_base+topic))
-        print(f"fetched in {time.time() - start_time} seconds")
-        success =True
-    else:
-        print("Unable to find the topic, try again\n")
-    out = False
-    if success:
-        soup = BeautifulSoup(response.text, 'html.parser')
+            # Extract title with header h1
+            h1_title = soup.find_all('h1')
+            # Extract all titles defined with header h2
+            h2_titles = soup.find_all('h2')
+            h2_titles[0]=h1_title[0]
 
-        # Extract title with header h1
-        h1_title = soup.find_all('h1')
-        # Extract all titles defined with header h2
-        h2_titles = soup.find_all('h2')
-        h2_titles[0]=h1_title[0]
-
-        while True:
-            print(f"\n{h1_title[0].text}\n")
-            for idx, title in enumerate(h2_titles):
-                print(str(idx)+'. ', 'Summary' if idx==0 else title.text)
-            next_topic = False
             while True:
-                idx = input('\nWhich chapter would you like to read?[q:quit,n:another topic] ')
-                if idx=='q':
-                    out = True
-                    break
-                elif idx=='n':
-                    next_topic = True
-                    break
-                elif re.match(r'^\d+$',idx)!=None:
-                    if len(h2_titles) > int(idx) and int(idx)>=0:
+                print(f"\n{h1_title[0].text}\n")
+                for idx, title in enumerate(h2_titles):
+                    print(str(idx)+'. ', 'Summary' if idx==0 else title.text)
+                next_topic = False
+                while True:
+                    idx = input('\nWhich chapter would you like to read?[q:quit,n:another topic] ')
+                    if idx=='q':
+                        out = True
                         break
+                    elif idx=='n':
+                        next_topic = True
+                        break
+                    elif re.match(r'^\d+$',idx)!=None:
+                        if len(h2_titles) > int(idx) and int(idx)>=0:
+                            break
+                        else:
+                            print("Wrong value, try again\n")
                     else:
                         print("Wrong value, try again\n")
-                else:
-                    print("Wrong value, try again\n")
-            if out or next_topic:
-                break
-            s = h2_titles[int(idx)]
-
-            text = ''
-            tmp  = s
-            if int(idx)==len(h2_titles)-1:
-                next_h2 = None
-            else:
-                next_h2 = h2_titles[int(idx)+1]
-
-            print('\n'+idx+'. '+h2_titles[int(idx)].text+":\n")
-
-            while True:
-                filters = ["p","h4","h3","h2"]
-                if int(idx)!=0:
-                    filters.append("ul")
-                tmp = tmp.find_next(filters)
-                
-                if tmp==next_h2:
+                if out or next_topic:
                     break
-                tmp_str = tmp.get_text().strip().replace('\n', ' ')
+                s = h2_titles[int(idx)]
 
-                reg_ppattern = r'\[\d+\]'
-                cleaned_paragraph= re.sub(reg_ppattern,'',tmp_str)
-                end = ' ' if (len(cleaned_paragraph)==0 or (cleaned_paragraph[-1]=='.')) else '. '
-                text += cleaned_paragraph + end
-            print(text)
+                text = ''
+                tmp  = s
+                if int(idx)==len(h2_titles)-1:
+                    next_h2 = None
+                else:
+                    next_h2 = h2_titles[int(idx)+1]
 
-            asyncio.run(main(text))
-    if out:
-        print("Have a nice day, human!")
-        break
+                print('\n'+idx+'. '+h2_titles[int(idx)].text+":\n")
+
+                while True:
+                    filters = ["p","h4","h3","h2"]
+                    if int(idx)!=0:
+                        filters.append("ul")
+                    tmp = tmp.find_next(filters)
+                    
+                    if tmp==next_h2:
+                        break
+                    tmp_str = tmp.get_text().strip().replace('\n', ' ')
+
+                    reg_ppattern = r'\[\d+\]'
+                    cleaned_paragraph= re.sub(reg_ppattern,'',tmp_str)
+                    end = ' ' if (len(cleaned_paragraph)==0 or (cleaned_paragraph[-1]=='.')) else '. '
+                    text += cleaned_paragraph + end
+                print(text)
+                voice = 'af_bella'
+                lang = 'en-us'
+                if language=='es':
+                    voice = 'em_santa'
+                    lang = 'es'
+                asyncio.run(reader(text, voice, lang))
+        if out:
+            print("Have a nice day, human!")
+            break
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    print(args)
+    language = 'en'
+    if len(args)==2:
+        if args[0]=='-l':
+            if args[1] in ['es','en']:
+                language = args[1]
+                main(language)
+            else:
+                print("Wrong argument, possible languages are 'es' and 'en'")
+        else:
+            print("Wrong argument, should be '-l'")
+    else:
+        if len(args)==0:
+            main(language)
+        else:
+            print('Incorrect number of arguments')
+
+    
+    
